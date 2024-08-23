d3b5d9c35632ea874c026f1897b7c0bffa0e0324
Aleksandar Stojanovic <coas91@rocketmail.com>
2024-08-23 22:14:04 +0200

    Build V50. Server. Import response changes from client.

--- src/rs/alexanderstojanovich/evgds/net/ResponseIfc.java
+++ src/rs/alexanderstojanovich/evgds/net/ResponseIfc.java
@@ -18,6 +18,7 @@ package rs.alexanderstojanovich.evgds.net;
 
 import java.util.concurrent.CompletableFuture;
 import org.apache.mina.core.buffer.IoBuffer;
+import org.apache.mina.core.future.ReadFuture;
 import org.apache.mina.core.session.IoSession;
 import rs.alexanderstojanovich.evgds.main.Game;
 import rs.alexanderstojanovich.evgds.main.GameServer;
@@ -66,7 +67,13 @@ public interface ResponseIfc extends DSObject {
      * @throws java.lang.Exception if network error
      */
     public static ResponseIfc receive(Game client, IoSession session) throws Exception {
-        Object message = session.read().getMessage();
+        ReadFuture read = session.read();
+        Object message = null;
+
+        // read message within specified timeout
+        if (read.await(client.getTimeout())) {
+            message = read.getMessage();
+        }
 
         // Get message as Byte Buffer
         if (message instanceof IoBuffer) {
