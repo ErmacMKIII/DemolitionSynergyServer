1d1d9179460180d14e87c6b870c6c952bbaa231f
Aleksandar Stojanovic <coas91@rocketmail.com>
2025-12-05 11:48:04 +0100

    Build V58.1. Server. Try fix model-not-update in Multiplayer.

--- src/rs/alexanderstojanovich/evgds/main/Window.java
+++ src/rs/alexanderstojanovich/evgds/main/Window.java
@@ -1682,7 +1682,7 @@ public class Window extends javax.swing.JFrame {
         URL icon_url = getClass().getResource(RESOURCES_DIR + LICENSE_LOGO_FILE_NAME);
         if (icon_url != null) {
             StringBuilder sb = new StringBuilder();
-            sb.append(String.format("VERSION v%s (%s BUILD reviewed on 2025-12-05 at 01:05).\n", GameObject.VERSION, BUILD.toString()));
+            sb.append(String.format("VERSION v%s (%s BUILD reviewed on 2025-12-05 at 08:30).\n", GameObject.VERSION, BUILD.toString()));
             sb.append("This software is free software, \n");
             sb.append("licensed under GNU General Public License (GPL).\n");
             sb.append("\n");
